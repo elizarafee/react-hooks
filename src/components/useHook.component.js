fb1f43ac7173c3927af27fe9e24ca1e5437aeb92
Eliza Ahmed <elizarafee@gmail.com>
2020-11-13 11:08:33 +0000

    usehook

---
+++ src/components/useHook.component.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+function UseHook(props) {
+    const [count, setCount] = useState(0);
+    
+    return (
+        <>
+            <button onClick={ () => setCount(count+1) }>{count}</button>
+        </>
+    )
+}
+
+export default UseHook;
