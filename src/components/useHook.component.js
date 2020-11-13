15d96264646e76fceea5fe507dcfafa7dffd72e8
Eliza Ahmed <elizarafee@gmail.com>
2020-11-13 11:26:44 +0000

    count is increamented by increamentcount arrow function

--- src/components/useHook.component.js
+++ src/components/useHook.component.js
@@ -2,10 +2,15 @@ import React, { useState } from 'react'
 
 function UseHook(props) {
     const [count, setCount] = useState(0);
+
+    const increamentCount = () => {
+        setCount(count+1);
+    };
     
     return (
         <>
-            <button onClick={ () => setCount(count+1) }>{count}</button>
+            {/* <button onClick={ () => setCount(count+1) }>{count}</button> */}
+            <button onClick={ increamentCount }>{count}</button>
         </>
     )
 }
