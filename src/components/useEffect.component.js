403822a789995b21b050cc30675790742ddcb291
Eliza Ahmed <elizarafee@gmail.com>
2020-11-15 15:46:20 +0000

    conditionally using use effect

--- src/components/useEffect.component.js
+++ src/components/useEffect.component.js
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from 'react'
 
 function UseEffectComponent(props) {
     const [count, setCount] = useState(0);
+    const [name, setName] = useState('');
 
     useEffect(() => {
+        console.log('reloaded');
         document.title = `Reloaded ${count} times`
-    })
+    }, [count])
 
     return (
         <>
+            <input type='txt' value={name} onChange={e => setName(e.target.value)}></input>
             <button onClick={() => setCount(count + 1)}>clicked {count} times</button>
         </>
     )
